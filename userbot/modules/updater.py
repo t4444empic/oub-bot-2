22769cd8c73bf46369f45265e05a3ce281931b24
GudMeong <59290656+GudMeong@users.noreply.github.com>
2020-02-06 20:20:04 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -26,7 +26,7 @@ async def gen_chlog(repo, diff):
 
 
 async def is_off_br(br):
-    off_br = ['sql-extended']
+    off_br = ['master']
     for k in off_br:
         if k == br:
             return 1
