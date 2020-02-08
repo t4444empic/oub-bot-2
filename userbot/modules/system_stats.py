138c8e626619b1801fc4bd18ea6496324a9c946f
GudMeong <59290656+GudMeong@users.noreply.github.com>
2020-02-08 15:05:19 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -132,27 +132,7 @@ async def amireallyalive(on):
                      "Hello everyone! \n"
                      "============="
 
-@register(outgoing=True, pattern="^.alive$")
-async def amireallyalive(alive):
-    """ For .alive command, check if the bot is running.  """
-    await alive.edit("`"
-                     "i am alive My Mastor \n\n"
-                     " \n\n"
-                     "botdo is alive \n\n"
-                     f"Telethon version: {version.__version__} \n"
-                     f"Python: {python_version()} \n"
-                     f"------------------------------------ \n"
-                     f"Website: https://www.facebook.com/Teknoways \n"
-                     " \n\n"
-                     f"User: {DEFAULTUSER} \n"
-                     " \n\n"
-                     f"Maintainer: @Mayur_Karaniya \n"
-                     " \n\n"
-                     f"Admin: @Three_Cube_TeKnoways \n"
-                     " \n\n"
-                     f"Userbot: @testing_userbot "
-
-                     "`")
+                                         "`")
 
 
 
