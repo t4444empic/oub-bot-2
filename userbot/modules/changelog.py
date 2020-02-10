e7f30063613193f8cc8ae84569c91fd16153ce37
GudMeong <avada758@gmail.com>
2020-02-10 10:29:51 +0700

    Update repo changelog // @Ucup77 buriQ

--- userbot/modules/changelog.py
+++ userbot/modules/changelog.py
@@ -37,8 +37,8 @@ async def is_off_br(br):
 async def chtream(ch):
     "For .update command, check if the bot is up to date, update if specified"
     await ch.edit("`Checking for updates, please wait....`")
-    conf = ch.pattern_match.group(1).lower()
-    off_repo = 'https://github.com/mkaraniya/OpenUserBot.git'
+   conf = ch.pattern_match.group(1).lower()
+    off_repo = 'https://github.com/GudMeong/oub-bot-2.git'
 
     try:
         txt = "`Oops.. Updater cannot continue due to some problems occured`\n\n**LOGTRACE:**\n"
