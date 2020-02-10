06da517f36106eb1fa455606767c66ef1a4cd726
GudMeong <avada758@gmail.com>
2020-02-10 22:14:20 +0700

    Disuruh @MoveAngel

--- userbot/modules/fakeload.py
+++ userbot/modules/fakeload.py
@@ -312,7 +312,7 @@ async def typewriter(typew):
 	sleep(0.03)
 	await typew.edit(str(number) + "%   ████████████████▌")
 	sleep(1)
-	await typew.edit("`Tokek Goreng Siap di Santap!`")
+	await typew.edit("Tokek Goreng Siap di Santap![Pesan DISINI](https://www.google.com/url?sa=t&source=web&rct=j&url=https://m.tokopedia.com/amp/joeluqmany/daging-tokek-gorengbakar-obat-gatal-asma-aids-dll&ved=2ahUKEwjI6bjeocfnAhXmyDgGHXp9CTYQFjADegQIChAB&usg=AOvVaw0JG7Fs8bPq_2rynIS_CtMF&ampcf=1)")
 	# I did it for two hours :D just ctrl+c - crtl+v
 
 
